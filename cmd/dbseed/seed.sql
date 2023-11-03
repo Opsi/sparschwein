df63ac03e82414681591c9fae73cfa1b7fc9d794
Robin Wiersig <Opsi@users.noreply.github.com>
2023-11-03 20:26:31 +0100

    feat: improves database model

--- cmd/dbseed/seed.sql
+++ cmd/dbseed/seed.sql
@@ -1,26 +1,64 @@
--- Script to create ledgers, transactions, and tags tables
-
--- Create the 'ledgers' table
-CREATE TABLE ledgers (
+-- Table for holders
+CREATE TABLE IF NOT EXISTS holders (
     id SERIAL PRIMARY KEY,
-    identifier VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    data JSON,
-    parent_id INTEGER REFERENCES ledgers(id) ON DELETE SET NULL
+    type VARCHAR(31),
+    identifier VARCHAR(255),
+    name VARCHAR(255),
+    parent_holder_id INT,
+    data JSONB,
+    favorite BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT unique_type_identifier UNIQUE (type, identifier),
+    CONSTRAINT fk_parent_holder FOREIGN KEY (parent_holder_id)
+        REFERENCES holders (id) ON DELETE SET NULL
 );
 
--- Create the 'transactions' table
-CREATE TABLE transactions (
+-- Table for transactions
+CREATE TABLE IF NOT EXISTS transactions (
     id SERIAL PRIMARY KEY,
-    from_ledger_id INTEGER REFERENCES ledgers(id) ON DELETE CASCADE,
-    to_ledger_id INTEGER REFERENCES ledgers(id) ON DELETE CASCADE,
-    amount_eur NUMERIC(12, 2) CHECK (amount_eur >= 0), -- Assuming no negative amounts are allowed
-    data JSON
+    from_holder_id INT NOT NULL,
+    to_holder_id INT NOT NULL,
+    amount INT,
+    timestamp TIMESTAMP,
+    data JSONB,
+    parent_transaction_id INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_from_holder FOREIGN KEY (from_holder_id)
+        REFERENCES holders (id) ON DELETE CASCADE,
+    CONSTRAINT fk_to_holder FOREIGN KEY (to_holder_id)
+        REFERENCES holders (id) ON DELETE CASCADE,
+    CONSTRAINT fk_parent_transaction FOREIGN KEY (parent_transaction_id)
+        REFERENCES transactions (id) ON DELETE SET NULL
 );
 
--- Create the 'tags' table
-CREATE TABLE tags (
+-- Table for tags
+CREATE TABLE IF NOT EXISTS tags (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    parent_id INTEGER REFERENCES tags(id) ON DELETE SET NULL
+    name VARCHAR(255),
+    parent_tag_id INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_parent_tag FOREIGN KEY (parent_tag_id)
+        REFERENCES tags (id) ON DELETE SET NULL
+);
+
+-- Many-to-many relation table between holders and tags
+CREATE TABLE IF NOT EXISTS holders_tags (
+    holder_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    CONSTRAINT fk_holder FOREIGN KEY (holder_id)
+        REFERENCES holders (id) ON DELETE CASCADE,
+    CONSTRAINT fk_tag FOREIGN KEY (tag_id)
+        REFERENCES tags (id) ON DELETE CASCADE,
+    CONSTRAINT pk_holders_tags PRIMARY KEY (holder_id, tag_id)
+);
+
+-- Many-to-many relation table between transactions and tags
+CREATE TABLE IF NOT EXISTS transactions_tags (
+    transaction_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    CONSTRAINT fk_transaction FOREIGN KEY (transaction_id)
+        REFERENCES transactions (id) ON DELETE CASCADE,
+    CONSTRAINT fk_tag_transaction FOREIGN KEY (tag_id)
+        REFERENCES tags (id) ON DELETE CASCADE,
+    CONSTRAINT pk_transactions_tags PRIMARY KEY (transaction_id, tag_id)
 );
