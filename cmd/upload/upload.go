392bf048b5d97a1c46efaa801f9bed642c7ac167
Robin Wiersig <Opsi@users.noreply.github.com>
2023-11-03 23:57:23 +0100

    feat: dry run saves json file

--- cmd/upload/upload.go
+++ cmd/upload/upload.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -31,13 +32,16 @@ func run() error {
 	var (
 		formatString = flag.String("format", "dkb", "what format the csv file is in")
 		filePath     = flag.String("file", "", "path to the csv file")
-		dryRun       = flag.Bool("dry", false, "dry run the script without writing to db")
+		dryFilePath  = flag.String(
+			"dry-file",
+			"",
+			"dry run the script and save the transactions and holders that would be created to the json file")
 	)
 	flag.Parse()
 	slog.Info("flags", slog.Group("flags",
 		slog.String("format", *formatString),
 		slog.String("file", *filePath),
-		slog.Bool("dry", *dryRun),
+		slog.String("dry-file", *dryFilePath),
 	))
 
 	if err := logConfig.InitSlogDefault(); err != nil {
@@ -67,13 +71,37 @@ func run() error {
 		return fmt.Errorf("parse csv: %w", err)
 	}
 
-	if *dryRun {
+	if *dryFilePath != "" {
 		// this is a dry run, so we just print the transactions
 		// and holders that would be created
+		// TODO: actually check which holders and transactions already exist
+		holderMap := make(map[db.HolderIdentifier]db.CreateHolder)
+		transactions := make([]db.CreateTransaction, 0)
 		for _, creator := range creators {
-			fmt.Printf("Transaction: %v\n", creator.Transaction())
-			fmt.Printf("From Holder: %v\n", creator.FromHolder())
-			fmt.Printf("To Holder: %v\n", creator.ToHolder())
+			transaction := creator.Transaction()
+			transactions = append(transactions, transaction)
+			fromHolder := creator.FromHolder()
+			toHolder := creator.ToHolder()
+			holderMap[fromHolder.HolderIdentifier] = fromHolder
+			holderMap[toHolder.HolderIdentifier] = toHolder
+		}
+		holders := make([]db.CreateHolder, 0)
+		for _, holder := range holderMap {
+			holders = append(holders, holder)
+		}
+		jsonData := struct {
+			Holders      []db.CreateHolder      `json:"holders"`
+			Transactions []db.CreateTransaction `json:"transactions"`
+		}{
+			Holders:      holders,
+			Transactions: transactions,
+		}
+		jsonBytes, err := json.Marshal(jsonData)
+		if err != nil {
+			return fmt.Errorf("marshal json: %w", err)
+		}
+		if err := os.WriteFile(*dryFilePath, jsonBytes, 0644); err != nil {
+			return fmt.Errorf("write json file: %w", err)
 		}
 	} else {
 		// this is not a dry run, so we create the transactions
